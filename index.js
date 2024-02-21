cbb274d7964f990300ad9f557ba102677ab7ecbc
Kristen <kgrace61@gmail.com>
2024-02-21 11:45:42 -0600

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = "bob"
+//write a function that accesses customerName and uppercases it
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer="not bob";
+}
+function overwriteBestCustomer() {
+    bestCustomer="maybe bob";
+}
+const leastFavoriteCustomer = "Sue";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Tom";
+}
